111ec190b4e83c0932cdc8bbf62e489b6855bd90
Delante Randolph <delantetr@MacBook-Pro.local>
2023-08-02 09:03:59 -0400

    added models

---
+++ models/Thought.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (value) => {
+          return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value);
+        },
+        message: 'Please enter a valid email address',
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+
+const User = model('User', userSchema);
+
+module.exports = User;
